2605662e34034aa8e7102ae1af9601352e114ac3
Sithija Shehara <nsithijashehara@gmail.com>
2020-05-05 13:23:44 +0530

    To-Do

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:to_do_app/Dash.dart';
+import 'package:to_do_app/Home.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:to_do_app/Test.dart';
+
+void main() {
+  FlutterError.onError = (FlutterErrorDetails details) {
+    FlutterError.dumpErrorToConsole(details);
+    if (kReleaseMode)
+      exit(1);
+  };
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My To~Do App',
+      home: Home()
+    );
+  }
+}
+
