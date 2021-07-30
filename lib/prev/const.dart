180171a2863cb7a78527db24f6c663c5dea8c1df
Sithija Shehara <nsithijashehara@gmail.com>
2021-07-30 12:18:06 +0530

    cleared previous files and initialized app

--- lib/prev/const.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static final Color backgroundColor = Color(0xFF90CAF9);
-  static final Color mainColor = Color(0xFF90CAF9);
-}
-//0xFF1565C0
