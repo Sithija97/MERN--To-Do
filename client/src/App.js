a611b28ee1eaefead372ff929c82a32fceed497e
Sithija Shehara <nsithijashehara@gmail.com>
2021-07-31 10:17:46 +0530

    Getting the data from the api and displaying it

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,42 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+const API_BASE = "http://localhost:5000";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [popupActive, setPopupActive] = useState(false);
+  const [newTodo, setNewTodo] = useState("");
+
+  const GetTodos = () => {
+    axios
+      .get(API_BASE + "/todos")
+      .then((data) => setTodos(data.data))
+      .catch((err) => console.error("Error: ", err));
+    console.log(todos);
+  };
+  useEffect(() => {
+    GetTodos();
+  }, []);
+
   return (
     <div className="App">
       <h1>Welcome, Sithija</h1>
       <h4>Your Tasks</h4>
 
       <div className="todos">
-        <div className="todo">
-          <div className="checkbox"></div>
-          <div className="text">Do the homework</div>
-          <div className="delete-todo">x</div>
-        </div>
-        <div className="todo is-complete">
+        {todos.map((todo) => (
+          <div key={todo._id} className="todo">
+            <div className="checkbox"></div>
+            <div className="text">{todo.text}</div>
+            <div className="delete-todo">x</div>
+          </div>
+        ))}
+
+        {/* <div className="todo is-complete">
           <div className="checkbox"></div>
           <div className="text">Do the Coding</div>
           <div className="delete-todo">x</div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
