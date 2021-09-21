aa5a9547583f4e45601df83f4836e2fffcc3b7f5
Sithija Shehara <nsithijashehara@gmail.com>
2021-09-21 18:15:40 +0530

    Completed the remainng create todo popup section

--- api/server.js
+++ api/server.js
@@ -1,46 +1,47 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 // import model
-const Todo = require('./models/Todo')   
+const Todo = require("./models/Todo");
 
 // endpoint
-app.get('/todos', async(req,res) => {
-    const todos = await Todo.find()
-    res.json(todos)
-})
+app.get("/todos", async (req, res) => {
+  const todos = await Todo.find();
+  res.json(todos);
+});
 
-app.post('/todo/new', (req, res) => {
-    const todo = new Todo({
-        text: req.body.text
-    })
-    todo.save()
-    res.json(todo)
-})
+app.post("/todo/new", (req, res) => {
+  const todo = new Todo({
+    text: req.body.text,
+  });
+  todo.save();
+  res.json(todo);
+});
 
-app.delete('/todo/delete/:id', async (req, res) => {
-    const result = await Todo.findByIdAndDelete(req.params.id)
-    res.json(result)
-})
+app.delete("/todo/delete/:id", async (req, res) => {
+  const result = await Todo.findByIdAndDelete(req.params.id);
+  res.json(result);
+});
 
-app.put('/todo/complete/:id', async (req, res) => {
-    const todo = await Todo.findById(req.params.id)
-    todo.complete = !todo.complete
-    todo.save()
-    res.json(todo)
-})
+app.get("/todo/complete/:id", async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
+  todo.complete = !todo.complete;
+  todo.save();
+  res.json(todo);
+});
 
-mongoose.connect('mongodb://127.0.0.1:27017/mern-todo', {
+mongoose
+  .connect("mongodb://127.0.0.1:27017/mern-todo", {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log("Connected to DB"))
-    .catch(console.error)
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to DB"))
+  .catch(console.error);
 
-app.listen(5000, ()=>console.log('Server started on port 5000'))
+app.listen(5000, () => console.log("Server started on port 5000"));
