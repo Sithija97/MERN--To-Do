d29f01ee17ff9a51710366da8b39c72feeaf3de8
Sithija Shehara <nsithijashehara@gmail.com>
2024-05-02 07:51:35 +0530

    register page UI

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -33,7 +33,7 @@ export const Login = () => {
       <div className=" flex items-center justify-center mt-28">
         <div className="w-96 border rounded bg-white px-7 py-10">
           <form onSubmit={handleLogin}>
-            <h4 className="text-xl mb-7">Login</h4>
+            <h4 className="text-xl mb-7">SignIn</h4>
             <input
               type="text"
               placeholder="Email"
