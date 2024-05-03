49516d0f3eb13fbfa0fde0f1320dbb9bbb9738a9
Sithija Shehara <nsithijashehara@gmail.com>
2024-05-03 23:20:41 +0530

    search bar UI

--- client/src/components/searchbar.tsx
+++ client/src/components/searchbar.tsx
@@ -1,3 +1,20 @@
-export const Searchbar = () => {
-  return <div>Searchbar</div>;
+import { ChangeEventHandler, FC } from "react";
+
+type IProps = {
+  value: string;
+  onChange: ChangeEventHandler;
+};
+
+export const Searchbar: FC<IProps> = ({ value, onChange }) => {
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search Notes"
+        className="w-full text-xs bg-transparent py-[11px] outline-none"
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
 };
