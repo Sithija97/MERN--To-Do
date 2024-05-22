014aa276734d0942f72c4e4bbdc4a5ebbe17d9ba
Sithija Shehara <nsithijashehara@gmail.com>
2024-05-22 23:56:00 +0530

    initialise api

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,6 @@
-// Import the 'express' module
+// Import modules
 import express from "express";
+import cors from "cors";
 
 // Create an Express application
 const app = express();
@@ -7,11 +8,11 @@ const app = express();
 // Set the port number for the server
 const port = 3000;
 
-// Define a route for the root path ('/')
-app.get("/", (req, res) => {
-  // Send a response to the client
-  res.send("Hello, TypeScript + Node.js + Express!");
-});
+// Middleware to parse incoming JSON requests
+app.use(express.json());
+
+// Middleware to enable Cross-Origin Resource Sharing (CORS) for all origins
+app.use(cors({ origin: "*" }));
 
 // Start the server and listen on the specified port
 app.listen(port, () => {
