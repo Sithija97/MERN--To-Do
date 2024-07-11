3012c38c90d7b6b5fd8b813872b338d14966d17c
Sithija Shehara <nsithijashehara@gmail.com>
2024-07-11 16:22:59 +0530

    UI components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,41 @@
+import { Button } from "./components/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "./components/ui/drawer";
+
 function App() {
-  return <></>;
+  return (
+    <section
+      className={`min-h-screen  ${
+        import.meta.env.VITE_DEV_ENV === "development" ? "debug-screens" : ""
+      }`}
+    >
+      test
+      <Drawer>
+        <DrawerTrigger>
+          <Button variant="outline">Open Drawer</Button>
+        </DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+            <DrawerDescription>This action cannot be undone.</DrawerDescription>
+          </DrawerHeader>
+          <DrawerFooter>
+            <DrawerClose>
+              <Button variant="outline">Cancel</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </section>
+  );
 }
 
 export default App;
