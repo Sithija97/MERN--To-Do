856b56c46ea9d8de90ff48c6fc63ce6138807631
Sithija Shehara <nsithijashehara@gmail.com>
2024-07-21 00:55:32 +0530

    added new atoms

---
+++ client/src/utils/index.tsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+import { Mail, mails } from "../data";
+
+type Config = {
+  selected: Mail["id"] | null;
+};
+
+const useMail = (): [Config, React.Dispatch<Config>] => {
+  const [mail, setMail] = useState<Config>({ selected: mails[0].id });
+
+  // Ensure initial selection remains consistent even on re-renders
+  useEffect(() => {
+    if (!mail.selected && mails.length > 0) {
+      setMail({ selected: mails[0].id });
+    }
+  }, [mails]); // Only re-run when mails data changes
+
+  return [mail, setMail];
+};
+
+export default useMail;
